bfc366044ebcc231f5d1c825a6a264557004243c
hynookim98 <hynookim98@gmail.com>
2022-12-17 13:47:06 -0800

    added sql files and created tables with seed data

---
+++ db/seed.sql
@@ -0,0 +1,24 @@
+USE employees;
+
+INSERT INTO department (name) 
+VALUES ('Sales'), ('Engineering'), ('Finance'), ('Legal');
+
+INSERT INTO role (title, salary, department_id) 
+VALUES ('Sales Lead', 100000, 1),
+       ('Salesperson', 80000, 1),
+       ('Lead Engineer', 150000, 2),
+       ('Software Engineer', 120000, 2),
+       ('Finance Manager', 160000, 3),
+       ('Accountant', 125000, 3),
+       ('Legal Team Lead', 250000, 4),
+       ('Lawyer', 500000, 4);
+
+INSERT INTO employee (first_name, last_name, role_id, manager_id) 
+VALUES ('Barry','Allen', 1, NULL),
+       ('John','Doe', 2, NULL),
+       ('Jane','Smith', 3, 2),
+       ('Harry','Potter', 4, 1),
+       ('Karen','Leksli', 5, NULL),
+       ('Danny','Phantom', 6, 1),
+       ('George','Ape', 7, NULL),
+       ('Stella','Artois', 8, NULL);
